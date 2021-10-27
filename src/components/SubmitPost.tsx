9bb6163dca69f668dfdd8f4a81bf27aca85f922d
cludRevature <92110713+cludRevature@users.noreply.github.com>
2021-10-27 09:17:43 -0700

    changed post component name to SubmitPost

---
+++ src/components/SubmitPost.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SubmitPost : React.FC = () => {
+    return(
+        <div>
+            Hello Create Submit Post Team!!!
+        </div>
+
+    );
+}
+
+export default SubmitPost;
