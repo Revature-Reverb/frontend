98b66e14726dd537f17737c40cff923302993568
cludRevature <92110713+cludRevature@users.noreply.github.com>
2021-10-27 11:46:58 -0700

    added boostrap card for form and input fields

--- src/components/SubmitPost.tsx
+++ src/components/SubmitPost.tsx
@@ -1,11 +1,30 @@
 import React from "react";
+import {Card, FloatingLabel, Form} from "react-bootstrap";
 
 const SubmitPost : React.FC = () => {
     return(
-        <div>
-            Hello Create Submit Post Team!!!
-        </div>
-
+        <Card>
+            <Card.Header>
+                <h1>Submit Post!</h1>
+            </Card.Header>
+            <Card.Body>
+                <Card.Title>
+                    What is on your mind?
+                </Card.Title>
+                <Form.Select>
+                    <option value="Text">Text</option>
+                    <option value="Photo">Photo</option>
+                    <option value="Link">Link</option>
+                </Form.Select>
+                <FloatingLabel controlId="floatingTextarea2" label="">
+                <Form.Control
+                    as="textarea"
+                    placeholder="What is on your mind today?"
+                    style={{ height: '100px', width: '300px' }}
+                />
+                </FloatingLabel>
+            </Card.Body>
+        </Card>
     );
 }
 
