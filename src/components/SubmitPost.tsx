1dbec2cdc36f5be28ec20ecb94e550c2573d08de
cludRevature <92110713+cludRevature@users.noreply.github.com>
2021-10-27 12:41:53 -0700

    cleaned up post submit and added button

--- src/components/SubmitPost.tsx
+++ src/components/SubmitPost.tsx
@@ -1,28 +1,26 @@
 import React from "react";
-import {Card, FloatingLabel, Form} from "react-bootstrap";
+import {Button, Card, Form} from "react-bootstrap";
 
 const SubmitPost : React.FC = () => {
     return(
         <Card>
             <Card.Header>
-                <h1>Submit Post!</h1>
+                <h1>New Post</h1>
             </Card.Header>
             <Card.Body>
                 <Card.Title>
                     What is on your mind?
-                </Card.Title>
+            </Card.Title>
                 <Form.Select>
                     <option value="Text">Text</option>
                     <option value="Photo">Photo</option>
                     <option value="Link">Link</option>
                 </Form.Select>
-                <FloatingLabel controlId="floatingTextarea2" label="">
                 <Form.Control
                     as="textarea"
                     placeholder="What is on your mind today?"
-                    style={{ height: '100px', width: '300px' }}
                 />
-                </FloatingLabel>
+                <Button type="submit">Post</Button>
             </Card.Body>
         </Card>
     );
