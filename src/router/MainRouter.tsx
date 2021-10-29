ad83b30694703bc3300def1b048a75f0d3c68ca1
Andres Rodriguez <andresmikaelrodriguez@gmail.com>
2021-10-29 12:09:28 -0700

    Login and registration through only firebase is implemented. Firebase dependency added, npm install to update project

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
-import {BrowserRouter as Router, Switch, Route, useHistory} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, useHistory } from "react-router-dom";
 import CreatePost from "../pages/createPost";
 import Feed from "../pages/feed";
 import Profile from "../pages/profile";
@@ -11,82 +11,81 @@ import Register from "../pages/register";
 import Friends from "../pages/friends";
 import Chat from "../pages/chat";
 import Settings from "../pages/settings";
-export default function MainRouter(props:any) {
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { logout } from '../slices/authSlice'
+export default function MainRouter(props: any) {
 
-    const [loggedIn, setLoggedIn] = useState(true);
+  const history = useHistory();
 
-    const history = useHistory();
+  // const [loggedIn, setLoggedIn] = useState(true);
 
-    const doLogout = () => {
-        
-        setLoggedIn(false)
-        history.push("/")
-    }
+  const loggedIn = useAppSelector(state => state.auth.authorized);
+  
+  const dispatch = useAppDispatch();
 
-    const doLogin = () => {
-        
-        setLoggedIn(true)
-        history.push("/")
-    }
-    let toReturn = <></>;
-    if(loggedIn)
-    {
-    toReturn = 
-        <div>
-            <Switch>
-                <Route path="/profile">
-                    <Profile/>
-                </Route>
-                <Route path="/create_post">
-                    <CreatePost/>
-                </Route>
-                <Route path="/search">
-                    <Search/>
-                </Route>
-                <Route path="/event">
-                    <Event/>
-                </Route>
-                <Route path="/chat">
-                    <Chat/>
-                </Route>
-                <Route path="/settings">
-                    <Settings/>
-                </Route>
-                <Route path="/friends">
-                    <Friends/>
-                </Route>
-                <Route path="/logout">
-                    {doLogout}
-                </Route>
-                <Route path="/">
-                    <Feed/>
-                </Route>
-            </Switch>
-        </div>
-    }
-    else {
-    toReturn = 
-        <div>
-            <Switch>
-                <Route path="/register">
-                    <Register/>
-                </Route>
-                <Route path="/create_post">
-                    <CreatePost/>
-                </Route>                
-                <Route path="/login">
-                <Login/>
-                    {/* {doLogin} */}
-                </Route>
-                <Route path="/">
-                    <Login/>
-                </Route>
-            </Switch>
-        </div>
-    }
-    return (
-        <>
-            {toReturn}
-        </>
-    )
+  const doLogout = () => {
+    dispatch(logout());
+    history.push("/")
+  }
+
+  let toReturn = <></>;
+  console.log("LoggedIn variable: ", loggedIn);
+  if (loggedIn) {
+    toReturn =
+      <div>
+        <Switch>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/create_post">
+            <CreatePost />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/event">
+            <Event />
+          </Route>
+          <Route path="/chat">
+            <Chat />
+          </Route>
+          <Route path="/settings">
+            <Settings />
+          </Route>
+          <Route path="/friends">
+            <Friends />
+          </Route>
+          <Route path="/logout">
+            {doLogout}
+          </Route>
+          <Route path="/">
+            <Feed />
+          </Route>
+        </Switch>
+      </div>
+  }
+  else {
+    toReturn =
+      <div>
+        <Switch>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/create_post">
+            <CreatePost />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+  }
+  return (
+    <>
+      {toReturn}
+    </>
+  )
 }
