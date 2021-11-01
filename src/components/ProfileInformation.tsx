a9c714bc936fe7e418d97b5605133ee9610d3609
unclebanks <49108377+unclebanks@users.noreply.github.com>
2021-11-01 00:03:44 -0700

    Added default image/banner
    * Added default Profile Image and Banner
    * Added default axios call to get user info?

--- src/components/ProfileInformation.tsx
+++ src/components/ProfileInformation.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect } from "react";
 import { Button, Card, Stack } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
+import defaultImage from '../blankUserIcon.png';
+import Logo from '../reverb_logo2.png';
 import axios from 'axios';
 
 let userName = 'Craig';
 let profileBody = 'This is just a placeholder for whatever is to come from the profile body'
+let imageSource = false;
 
 export default function ProfileInformation() {
     const history = useHistory();
@@ -17,16 +20,30 @@ export default function ProfileInformation() {
         history.push("/editProfile");
     }
     function getProfileInformation() {
-        console.log('axios call needed here');
+        /*
+        let userId = 1
+        // ^^^ Needs to be real id or something?
+        let axiosUrl = `http://localhost:8080/api/profile/${userId}`
+        axios
+            .get(axiosUrl)
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch((err) => {
+                console.log({err});
+                alert(err.response);
+            });
+        */
+       console.log("axios call is commented in the code");
     }
     return(
-        <Card style={{ width: '25rem', border: '2px solid gray' }}>
+        <Card style={{ border: '2px solid gray' }}>
             <Stack direction={"horizontal"} gap={1}>
-                <Card.Img style={{ border: '2px solid black', height:'96px', width:'50%', backgroundColor: 'coral'}}/>
-                <Card.Img style={{ border: '2px solid blue', height:'96px', backgroundColor: 'purple'}}/>
+                <Card.Img src={imageSource ? 'refactor for image' : defaultImage } style={{ border: '2px solid black', height:'96px', width:'50%'}}/>
+                <Card.Img src={Logo} style={{ border: '2px solid blue', height:'96px'}}/>
             </Stack>
             <Card.Body>
-                <Card.Title style={{ textAlign:'center' }}>Hello {userName}!</Card.Title>
+                <Card.Title style={{ textAlign:'center' }}>{userName}!</Card.Title>
                 <Card.Text style={{ backgroundColor: 'skyblue'}}>
                 {profileBody}
                 </Card.Text>
