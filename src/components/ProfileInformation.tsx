0a64c9e28c3b74018a0cb67935fc9517876563cf
unclebanks <49108377+unclebanks@users.noreply.github.com>
2021-11-02 11:49:35 -0700

    Update usercall

--- src/components/ProfileInformation.tsx
+++ src/components/ProfileInformation.tsx
@@ -3,47 +3,42 @@ import { Button, Card, Stack } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import defaultImage from '../blankUserIcon.png';
 import Logo from '../reverb_logo2.png';
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser } from "../slices/userSlice";
+import { getUserAsync } from '../slices/userSlice';
 import axios from 'axios';
+import {User} from '../models/userModel';
 
 let userName = 'Craig';
 let profileBody = 'This is just a placeholder for whatever is to come from the profile body'
 let imageSource = false;
 
 export default function ProfileInformation() {
+    const user = useSelector(selectUser);
+    const dispatch = useDispatch();
     const history = useHistory();
+    let dud: User= {
+        userID: 0,
+        username: "",
+        firstName: "",
+        lastName: ""
+    };
 
     useEffect(() => {
-        getProfileInformation();
+        dispatch(getUserAsync(dud));
       });
 
     const goToEditProfile = () => {
         history.push("/editProfile");
     }
-    function getProfileInformation() {
-        /*
-        let userId = 1
-        // ^^^ Needs to be real id or something?
-        let axiosUrl = `http://localhost:8080/api/profile/${userId}`
-        axios
-            .get(axiosUrl)
-            .then((res) => {
-                console.log(res.data);
-            })
-            .catch((err) => {
-                console.log({err});
-                alert(err.response);
-            });
-        */
-       console.log("axios call is commented in the code");
-    }
     return(
-        <Card style={{ border: '2px solid gray' }}>
+        <Card style={{ border: '2px solid gray', width:'50%' }}>
             <Stack direction={"horizontal"} gap={1}>
                 <Card.Img src={imageSource ? 'refactor for image' : defaultImage } style={{ border: '2px solid black', height:'96px', width:'50%'}}/>
                 <Card.Img src={Logo} style={{ border: '2px solid blue', height:'96px'}}/>
             </Stack>
             <Card.Body>
-                <Card.Title style={{ textAlign:'center' }}>{userName}!</Card.Title>
+                <Card.Title style={{ textAlign:'center' }}>{user.username}!</Card.Title>
                 <Card.Text style={{ backgroundColor: 'skyblue'}}>
                 {profileBody}
                 </Card.Text>
