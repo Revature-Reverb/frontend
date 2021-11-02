66e2410288714b75e70adf5382006fc28eef6538
jkoh2020 <jkoh123@yahoo.com>
2021-11-02 17:52:08 -0400

    Modified EditProfile and EditProfilePage

--- src/components/EditProfile.tsx
+++ src/components/EditProfile.tsx
@@ -1,5 +1,6 @@
-import { Grid } from '@material-ui/core';
+import { Grid, TextField } from '@material-ui/core';
 import { useState } from 'react';
+import { Card } from 'react-bootstrap';
 import { Link, useHistory } from 'react-router-dom';
 import "./EditProfile.css";
 
@@ -37,41 +38,42 @@ function EditProfile() {
 
     return (
         <div>
-            <form className="card">
+             <Grid container direction="column" alignItems="center" justify="center">
+            <Card>
                  <div className="form_input-group">
                     <label htmlFor="first_name">First Name</label>
-                    <input className="form_input" type="text" name="firstName" placeholder="John" value={input.first_name} 
+                    <input className="form_input" type="text" name="first_name" placeholder="John" value={input.first_name} 
                     onChange={handleChange} required />
                 </div>
 
                 <div className="form_input-group">
                     <label htmlFor="last_name">Last Name</label>
-                    <input className="form_input" type="textbox" name="lastName" placeholder="Kim"  value={input.last_name} 
+                    <input className="form_input" type="textbox" name="last_name" placeholder="Kim"  value={input.last_name} 
                     onChange={handleChange} required />
                 </div>
 
                 <div className="form_input-group">
                     <label htmlFor="profile_img">Select Profile Image</label>
-                    <input className="form_input" type="text" name="profileImg" placeholder="Profile Image url"  value={input.profile_img} 
+                    <input className="form_input" type="text" name="profile_img" placeholder="Profile Image url"  value={input.profile_img} 
                     onChange={handleChange} required />
                 </div>
 
                 <div className="form_input-group">
                     <label htmlFor="header_img">Select Header Image</label>
-                    <input className="form_input" type="text" name="headerImg" placeholder="Header Image url"  value={input.header_img}
+                    <input className="form_input" type="text" name="header_img" placeholder="Header Image url"  value={input.header_img}
                     onChange={handleChange} required />
                 </div>
 
                 <div className="form_input-group">
                     <label htmlFor="about_me">About Me</label>
-                    <input className="form_input" type="text" name="about_me" placeholder="I am a software developer"  value={input.about_me}
-                    onChange={handleChange} required pattern="\S+"/>
+                    <textarea className="form_input" rows={5} name="about_me" placeholder="I am a software developer"  value={input.about_me}
+                    onChange={handleChange}  > </textarea>
                 </div>
 
                 <button className="btn_color" type="submit" onClick={update} >Update</button><br />
                 <button className="cancel_color" type="submit" onClick={cancel} >Cancel</button><br />
-                
-            </form>
+            </Card>
+            </Grid>
         </div>
     )
 
--- src/pages/EditProfilePage.tsx
+++ src/pages/EditProfilePage.tsx
@@ -1,12 +1,15 @@
 import { Grid } from "@material-ui/core";
 import React from "react";
+import { Row } from "react-bootstrap";
 import EditProfile from "../components/EditProfile";
 
 export default function EditProfilePage() {
 
     return(
         <div>
-            <EditProfile />    
+            <Row>
+                <EditProfile />    
+            </Row>
         </div>
     )
 }
