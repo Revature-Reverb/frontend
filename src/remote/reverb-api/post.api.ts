25f5fdd4a80ac343b07571453b72a9989856cdd0
tbridey <taelormcbride@icloud.com>
2021-11-03 15:07:18 -0600

    changed to using axiosWithAuth

--- src/remote/reverb-api/post.api.ts
+++ src/remote/reverb-api/post.api.ts
@@ -1,14 +1,14 @@
 import { Post } from "../../models/postModel";
-import reverbClient from './reverbClient'
+import { reverbClient, reverbClientWithAuth } from './reverbClient'
 
 export const createPost = async (neoPost: Post): Promise<Post> => {
-  const {data: post} = await reverbClient.post<Post>('/api/post', neoPost);
+  const {data: post} = await reverbClientWithAuth.post<Post>('/api/post', neoPost);
 
   return post;
 }
 
 export const getPost = async (): Promise<Post> => {
-  const {data: post} = await reverbClient.get<Post>('/api/post/testNoAuth');
+  const {data: post} = await reverbClientWithAuth.get<Post>('/api/post/testNoAuth');
 
   return post;
 }
