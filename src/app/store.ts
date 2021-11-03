f5a394b13ee6c63f382507f01b8d72cd73652dc7
Max Nguyen <max.nguyen@revature.net>
2021-11-03 14:56:45 -0700

    finished front to back end profile get method and display

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import userReducer from '../slices/userSlice';
-import authReducer from '../slices/authSlice'
+import authReducer from '../slices/authSlice';
+import profileReducer from '../slices/profileSlice';
 
 
 
@@ -9,7 +10,8 @@ export const store = configureStore({
   reducer: {
     counter: counterReducer,
     user: userReducer,
-    auth: authReducer
+    auth: authReducer,
+    profile: profileReducer
   }
 });
 
