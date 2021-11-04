220e4c0adcb9dfe2e1b4b79bad560385f964c367
JasonC9 <jasonlam959@gmail.com>
2021-11-04 16:46:44 -0400

    landing page

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
-
+import {Button,Container, Card} from 'react-bootstrap'
+import Logo from '../rev-logo.png'
 const Landing = ()=> {
 
     const toReturn = <>Landing Page</>
 
     return(
-        <>{toReturn}</>
+        <>
+            <div>
+            <Container fluid="md">
+                <img
+                    alt=""
+                    src={Logo}
+                    width="180"
+                    height="70"
+                    className="d-inline-block align-top"
+                />
+                <br/>
+                <br/>
+                <Card style={{width:"600px",height:"250px",borderWidth:"10px",borderRadius:"10px", borderColor:"#B9B9BA",backgroundColor:"#B9B9BA"}}>
+                    <Card.Title>Reverb</Card.Title>
+                <Card.Text>
+                    Reverb is a new social media for users that allow you to collaborate and communicate with other users. 
+                    Users can customize their “Stage” user profiles and listen in to other users’ posts from around the world. 
+                    If someone’s post really reverberates with a user, they can “ReverB” it or just give it a passing vibe check. 
+                </Card.Text>
+                </Card>
+                </Container>
+            </div>
+            </>
     )
 }
 
