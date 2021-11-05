7bec7958317d24d7ed9b91b65da8f7476f117021
Terrell Miller <Terrell.Miller@revature.net>
2021-11-04 18:18:30 -0700

    resolve conflicts when they arise by accepting incomming

--- src/components/SubmitComment.tsx
+++ src/components/SubmitComment.tsx
@@ -1,24 +1,9 @@
 import React, {useEffect, useState} from "react";
 import { useDispatch } from 'react-redux'
 import {Button, Form } from "react-bootstrap";
-import { commmentCommentAysc } from "../slices/commentSlice";
+
 
 const SubmitComment : React.FC = () => {
-    const [comment, setComment] = useState({});
-
-    let initialState: PostState  = {
-        text: ""
-    }
-    const dispatch = new useDispatch();
-
-    function dispatchComment = (event: ReactFormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-
-        dispatch(commmentCommentAysc(comment));
-
-        return comment
-
-    }
     return(
         //start of form to create a comment
         <Form> 
--- src/remote/reverb-api/comment.api.ts
+++ src/remote/reverb-api/comment.api.ts
@@ -1,15 +1,2 @@
 import { Comment } from "../../models/commentModel";
-import reverbClient from './reverbClient'
-
-export const createComment = async (neoUser: Comment): Promise<Comment> => {
-  const {data: comment} = await reverbClient.post<Comment>('/api/user', neoUser);
-
-  return comment;
-}
-
-export const getComment = async (): Promise<Comment> => {
-  const {data: comment} = await reverbClient.get<Comment>('/api/user/testNoAuth');
-
-  return comment;
-}
 
