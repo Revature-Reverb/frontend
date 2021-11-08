7cb1d8d9d41f26cf23a561cfeab105b6dfe78519
cludRevature <92110713+cludRevature@users.noreply.github.com>
2021-11-08 13:09:29 -0800

    updated landing.tsx reverb description and added new logos

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,33 +1,39 @@
 import React from 'react'
-import { Container, Card } from 'react-bootstrap'
-import Logo from '../assets/images/rev-logo.png'
+import { Container, Row, Col, Card, Button } from 'react-bootstrap'
+import Logo from '../assets/images/reverb_logo2.png'
+import RevatureLogo from '../assets/images/by_rev_logo.png'
+
 const Landing = ()=> {
 
     const toReturn = <>Landing Page</>
 
     return(
         <>
-            <div>
-            <Container fluid="md">
+            <Container fluid="md" className="">
                 <img
-                    alt=""
+                    alt="Reverb Logo"
                     src={Logo}
-                    width="180"
-                    height="70"
+                    width="540"
+                    height="210"
                     className="d-inline-block align-top"
                 />
                 <br/>
-                <br/>
-                <Card style={{width:"600px",height:"250px",borderWidth:"10px",borderRadius:"10px", borderColor:"#B9B9BA",backgroundColor:"#B9B9BA"}}>
-                    <Card.Title>Reverb</Card.Title>
-                <Card.Text>
-                    Reverb is a new social media for users that allow you to collaborate and communicate with other users.
-                    Users can customize their “Stage” user profiles and listen in to other users’ posts from around the world.
-                    If someone’s post really reverberates with a user, they can “ReverB” it or just give it a passing vibe check.
-                </Card.Text>
-                </Card>
-                </Container>
-            </div>
+                <img 
+                alt="Revature Logo"
+                src={RevatureLogo}
+                width="210"
+                />
+                <Row className="justify-content-md-center">
+                    <Col md="6">
+                        <Card id="landingCard">
+                            <Card.Text>
+                            <i>Reverb</i> is social network for Revature Employees! Users can customize their user profiles and view a stream of Revature posts from around the world.
+                            If someone’s post really reverberates with a user, they can “ReverB” it! 
+                            </Card.Text>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
