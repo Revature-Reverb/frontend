0aae12d6c77d3dbf96a0fe92b942770c8d709677
tbridey <taelormcbride@icloud.com>
2021-11-08 09:26:21 -0700

    comments and font imports on app.css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import {Nav, NavLink, Form} from 'react-bootstrap'
 import Navbar from 'react-bootstrap/Navbar'
-import Logo from '../assets/images/reverb_logo2.png'
+import Logo from '../assets/images/reverb_logo_final_light.png'
 //import RevLogo from '../rev-logo.png'
 import ProfileImage from '../assets/images/blankUserIcon.png'
 import { Link, useHistory } from 'react-router-dom'
